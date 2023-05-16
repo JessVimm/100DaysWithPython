e13f50c24c9acb6c35051036795cc890aa20f883
JessVimm <jessvimm@gmail.com>
2023-05-15 18:03:55 -0600

    add code for day 6 part 1

---
+++ 006PyDay/006Py_part1.py
@@ -0,0 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+right_turns_made = 0
+    
+while not at_goal():
+    if not wall_on_right():
+        if right_turns_made < 4:
+            right_turns_made += 1
+            turn_right()
+            move()
+        else:
+            turn_left()
+            right_turns_made = 0
+    elif front_is_clear():
+        move()
+        right_turns_made = 0
+    else:
+        turn_left()
+        right_turns_made = 0
