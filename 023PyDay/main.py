9d7655d1d333d9f5ce211e0c51aeb0e82921cbe7
JessVimm <jessvimm@gmail.com>
2023-06-13 23:43:33 -0600

    setting the screen for the ping pong game on day 23

---
+++ 023PyDay/main.py
@@ -0,0 +1,13 @@
+# Ping pong game!
+from turtle import Screen
+
+# Create a screen object
+screen = Screen()
+
+# Screen setup
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+
+# Exit on click
+screen.exitonclick()
